5770b3176a225d8f94b6233d6c31b0c233e73774
urmila <urmilapradeep@gmail.com>
2018-01-30 22:41:37 -0800

    Update dependencies

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -23,4 +20,3 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
